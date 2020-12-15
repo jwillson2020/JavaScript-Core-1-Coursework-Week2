118221fe2328cab68c6bff392b9dcde63e8a91ac
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-15 15:39:48 +0000

    Done E
    added jane and student

--- exercises/E-conditionals/exercise.js
+++ exercises/E-conditionals/exercise.js
@@ -6,8 +6,27 @@
   If Daniel is a student, print out "Hi, I'm Daniel, I'm a student."
 */
 
-var name = "Daniel";
+var name = "Daniel"; 
 var danielsRole = "mentor";
+var name2 = "Jane";
+var janeRole ="student";
+if ("Daniel") {
+
+  console.log ("Hi, I'm " + name);
+}
+
+//if (student) {
+ // return "Hi, I'm" + name;
+ // }
+
+if ("mentor") {
+console.log( "I'm a mentor.");
+}
+
+if ( "Jane" , "student") {
+ console.log("Hi, I'm " + name2 + " I'm a student.");
+ }
+
 
 /* 
 EXPECTED RESULT
