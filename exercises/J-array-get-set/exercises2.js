bf281b916481112d22bdf56f17643b7ef3d0dae9
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-16 17:22:58 +0000

    Done J
    played with push and shift unshift

--- exercises/J-array-get-set/exercises2.js
+++ exercises/J-array-get-set/exercises2.js
@@ -7,7 +7,9 @@
 */
 
 var numbers = [1, 2, 3]; // Don't change this array literal declaration
-
+numbers.push(4); // used push adds 4 to end of array
+//numbers.shift(); // remove first number
+numbers.unshift(0);  // adds 0 to the front of 
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
