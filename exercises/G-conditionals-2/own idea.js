13807ca64eda8a98298d2bc362fe9ea2175a811c
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2020-12-16 16:12:51 +0000

    Done G
    just arrays weird??
    
    no exercise

--- exercises/G-conditionals-2/own idea.js
+++ exercises/G-conditionals-2/own idea.js
@@ -2,14 +2,18 @@
 
 function containsArt(sentence) {
     if (sentence.includes("draw") || sentence.includes("design")){ 
-     return "art search/n";
+     return "art in search data\n";
 
+}
+
+else {
+return "not art related\n";
 }
 }
 var sentence1 = "code your future";
 var sentence2 = "draw your future";
 var sentence3 = "design your future";
 
-console.log("'" + sentence1 + "': " + containsCode(sentence1));
-console.log("'" + sentence2 + "': " + containsCode(sentence2));
-console.log("'" + sentence3 + "': " + containsCode(sentence3));
+console.log("'" + sentence1 + "': " + containsArt(sentence1));
+console.log("'" + sentence2 + "': " + containsArt(sentence2));
+console.log("'" + sentence3 + "': " + containsArt(sentence3));
